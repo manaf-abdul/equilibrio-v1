0dae7638cb8a84c2cd3616b3c8cc84d257c6697a
Abdul Manaf <abdulmanafp1996@gmail.com>
2024-10-20 02:22:23 +0530

    FEAT : HERO SECTION

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,40 @@ import DefaultLayout from "@/layouts/default";
 export default function IndexPage() {
   return (
     <DefaultLayout>
-      {/*
-  Heads up! 👋
+      <section className="bg-gray-50">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
+            <h1 className="text-3xl font-extrabold sm:text-5xl">
+              Design Your Dreams
+              <strong className="font-extrabold text-red-700 sm:block">
+                {" "}
+                Equilibrio Designs.{" "}
+              </strong>
+            </h1>
 
-  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
-*/}
+            <p className="mt-4 sm:text-xl/relaxed">
+              Elevating Spaces, Designing Dreams , Crafting Legacies & Build
+              Future with Equilibrio
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                href="/projects"
+              >
+                Get Started
+              </a>
+
+              <a
+                className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                href="/contact-us"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
         <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
